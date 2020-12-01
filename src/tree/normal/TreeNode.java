1dfa7394ca7d04b7647051a207153590317c8547
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2020-12-01 15:35:16 +0800

    107. 二叉树的层次遍历 II

--- src/tree/normal/TreeNode.java
+++ src/tree/normal/TreeNode.java
@@ -56,4 +56,42 @@ public class TreeNode {
         }
         return res;
     }
+
+    /**
+     * 107. 二叉树的层次遍历 II
+     */
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        //or
+//        LinkedList<List<Integer>> res2 = new LinkedList<>();
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        if (root != null){
+            queue.add(root);
+        }
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i ++){
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null){
+                    queue.add(node.left);
+                }
+                if (node.right != null){
+                    queue.add(node.right);
+                }
+            }
+            res.add(list);
+            //or
+//            res2.addFirst(list);
+        }
+        int length = res.size();
+        List<List<Integer>> lists = new ArrayList<>();
+        for (int j = length-1; j >= 0; j--){
+            lists.add(res.get(j));
+        }
+        return lists;
+        //or
+//        return res2;
+    }
 }
