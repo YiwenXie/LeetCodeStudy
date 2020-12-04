bdf7c20d20df2f32b33cbd50f8275421c71bd46b
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2020-12-04 16:07:55 +0800

    116. 填充每个节点的下一个右侧节点指针 BFS与DFS解法

--- src/tree/normal/TreeNode.java
+++ src/tree/normal/TreeNode.java
@@ -327,6 +327,71 @@ public class TreeNode {
         largestValuesDFS(node.right, depth+1, list);
     }
 
-    //116.填充每个节点的下一个右侧节点指针
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    /**
+     * 116. 填充每个节点的下一个右侧节点指针
+     * BFS
+     */
+    public Node connectByBFS(Node root) {
+        if (root == null){
+            return root;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            for (int i = 0; i < size; i++){
+                Node node = queue.poll();
+                if (i < size - 1) {
+                    node.next = queue.peek();
+                }
+                if (node.left != null){
+                    queue.add(node.left);
+                }
+                if (node.right != null){
+                    queue.add(node.right);
+                }
+            }
+        }
+        return root;
+    }
+
+    /**
+     * 116. 填充每个节点的下一个右侧节点指针
+     * DFS
+     */
+    public Node connectByDFS(Node root) {
+        if (root == null){
+            return root;
+        }
+        if(root.left!=null){
+            root.left.next=root.right;
+            root.right.next=root.next!=null?root.next.left:null;
+            connectByDFS(root.left);
+            connectByDFS(root.right);
+        }
+        return root;
+    }
+
     //117.填充每个节点的下一个右侧节点指针II
 }
