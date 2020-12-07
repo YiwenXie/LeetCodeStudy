ab1e3e3f0dab8e21f076657d2c02e4d13dea01e5
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2020-12-07 14:24:25 +0800

    105. 从前序与中序遍历序列构造二叉树 递归+迭代解法

--- src/tree/normal/TreeNode.java
+++ src/tree/normal/TreeNode.java
@@ -421,9 +421,63 @@ public class TreeNode {
         return root;
     }
 
-    public Node connect2ByDFS(Node root) {
-        if (root == null){
-            return root;
+    /**
+     * 105. 从前序与中序遍历序列构造二叉树
+     * 递归
+     */
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return buildTreeHelper(preorder, 0, preorder.length, inorder, 0, inorder.length);
+    }
+
+    private TreeNode buildTreeHelper(int[] preorder, int p_start, int p_end, int[] inorder, int i_start, int i_end) {
+        // preorder 为空，直接返回 null
+        if (p_start == p_end) {
+            return null;
+        }
+        int root_val = preorder[p_start];
+        TreeNode root = new TreeNode(root_val);
+        //在中序遍历中找到根节点的位置
+        int i_root_index = 0;
+        for (int i = i_start; i < i_end; i++) {
+            if (root_val == inorder[i]) {
+                i_root_index = i;
+                break;
+            }
+        }
+        int leftNum = i_root_index - i_start;
+        //递归的构造左子树
+        root.left = buildTreeHelper(preorder, p_start + 1, p_start + leftNum + 1, inorder, i_start, i_root_index);
+        //递归的构造右子树
+        root.right = buildTreeHelper(preorder, p_start + leftNum + 1, p_end, inorder, i_root_index + 1, i_end);
+        return root;
+    }
+
+    /**
+     * 105. 从前序与中序遍历序列构造二叉树
+     * 迭代
+     */
+    public TreeNode buildTree2(int[] preorder, int[] inorder) {
+        if (preorder == null || preorder.length == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[0]);
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        stack.push(root);
+        int inorderIndex = 0;
+        for (int i = 1; i < preorder.length; i++) {
+            int preorderVal = preorder[i];
+            TreeNode node = stack.peek();
+            if (node.val != inorder[inorderIndex]) {
+                node.left = new TreeNode(preorderVal);
+                stack.push(node.left);
+            } else {
+                while (!stack.isEmpty() && stack.peek().val == inorder[inorderIndex]) {
+                    node = stack.pop();
+                    inorderIndex++;
+                }
+                node.right = new TreeNode(preorderVal);
+                stack.push(node.right);
+            }
         }
         return root;
     }
