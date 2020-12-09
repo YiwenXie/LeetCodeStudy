c7d4ab59272e1844f2a62da48ed70ae11aab54f7
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2020-12-09 16:26:54 +0800

    116. 填充每个节点的下一个右侧节点指针 DFS另一种通俗易懂的方式

--- src/tree/normal/TreeNode.java
+++ src/tree/normal/TreeNode.java
@@ -397,6 +397,34 @@ public class TreeNode {
     }
 
     /**
+     * 116. 填充每个节点的下一个右侧节点指针
+     * DFS2
+     */
+    public Node connectByDFS2(Node root) {
+        if (root == null) {
+            return null;
+        }
+        connectTwoNode(root.left, root.right);
+        return root;
+    }
+
+    // 辅助函数
+    private void connectTwoNode(Node node1, Node node2) {
+        if (node1 == null || node2 == null) {
+            return;
+        }
+        /**** 前序遍历位置 ****/
+        // 将传入的两个节点连接
+        node1.next = node2;
+
+        // 连接相同父节点的两个子节点
+        connectTwoNode(node1.left, node1.right);
+        connectTwoNode(node2.left, node2.right);
+        // 连接跨越父节点的两个子节点
+        connectTwoNode(node1.right, node2.left);
+    }
+
+    /**
      * 117.填充每个节点的下一个右侧节点指针II
      * BFS
      */
