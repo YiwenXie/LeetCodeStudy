69561fe80da047af8d7f92f34d4a68e538d39a34
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2020-12-16 14:33:47 +0800

    108. 将有序数组转换为二叉搜索树 中序遍历构造高度平衡二叉搜索树

--- src/tree/normal/TreeNode.java
+++ src/tree/normal/TreeNode.java
@@ -860,4 +860,26 @@ public class TreeNode {
         }
         return node;
     }
+
+    /**
+     * 108. 将有序数组转换为二叉搜索树
+     * 中序遍历构造二叉树
+     */
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums == null || nums.length == 0){
+            return null;
+        }
+        return sortedArrayToBSTHelper(nums, 0, nums.length-1);
+    }
+
+    private TreeNode sortedArrayToBSTHelper(int[] nums, int low, int high){
+        if (low > high) {
+            return null;
+        }
+        int mid = low + (high - low) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = sortedArrayToBSTHelper(nums, low, mid-1);
+        node.right = sortedArrayToBSTHelper(nums, mid+1, high);
+        return node;
+    }
 }
