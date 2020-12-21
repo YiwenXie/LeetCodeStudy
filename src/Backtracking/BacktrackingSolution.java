a9246f1cb9c23e7cf37b9c3c82e31f65fbedcd4e
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2020-12-21 15:52:49 +0800

    297. 二叉树的序列化与反序列化 增加注释

--- src/Backtracking/BacktrackingSolution.java
+++ src/Backtracking/BacktrackingSolution.java
@@ -22,6 +22,7 @@ public class BacktrackingSolution {
      * 46. 全排列
      * 后序遍历
      */
+    /* 主函数，输入一组不重复的数字，返回它们的全排列 */
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new LinkedList<>();
         LinkedList<Integer> track = new LinkedList<>();
@@ -29,17 +30,25 @@ public class BacktrackingSolution {
         return res;
     }
 
+    // 路径：记录在 track 中
+    // 选择列表：nums 中不存在于 track 的那些元素
+    // 结束条件：nums 中的元素全都在 track 中出现
     private void permuteHelper(int[] nums, LinkedList<Integer> track, List<List<Integer>> res){
+        // 触发结束条件
         if (track.size() == nums.length){
             res.add(new LinkedList<>(track));
             return;
         }
         for (int i = 0; i < nums.length; i++){
+            // 排除不合法的选择
             if (track.contains(nums[i])){
                 continue;
             }
+            // 做选择
             track.add(nums[i]);
+            // 进入下一层决策树
             permuteHelper(nums, track, res);
+            // 取消选择
             track.removeLast();
         }
     }
