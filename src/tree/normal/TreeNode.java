fd83511b065f8015875145d681d2b95778f5c87b
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2020-12-22 16:41:41 +0800

    112. 路径总和

--- src/tree/normal/TreeNode.java
+++ src/tree/normal/TreeNode.java
@@ -1121,4 +1121,17 @@ public class TreeNode {
         node.right = deserializeHelper(list);
         return node;
     }
+
+    /**
+     * 112. 路径总和
+     */
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null){
+            return false;
+        }
+        if (root.left == null && root.right == null){
+            return sum - root.val == 0;
+        }
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
 }
