e8883d77ddca163eaa05735a28d520a97bcf61dc
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2020-12-23 16:14:54 +0800

    235. 二叉搜索树的最近公共祖先

--- src/tree/normal/TreeNode.java
+++ src/tree/normal/TreeNode.java
@@ -1271,5 +1271,16 @@ public class TreeNode {
         }
     }
 
-
+    /**
+     * 235. 二叉搜索树的最近公共祖先
+     */
+    public TreeNode lowestCommonAncestor1(TreeNode root, TreeNode p, TreeNode q) {
+        if ((root.val - p.val) * (root.val - q.val) <= 0){
+            return root;
+        }else if (root.val < p.val && root.val < q.val){
+            return lowestCommonAncestor1(root.right, p, q);
+        }else {
+            return lowestCommonAncestor1(root.left, p, q);
+        }
+    }
 }
