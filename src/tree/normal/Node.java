a8064a574376ee1177f9e2382ec8be1024aee9e1
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2021-01-04 14:26:21 +0800

    589. N叉树的前序遍历

--- src/tree/normal/Node.java
+++ src/tree/normal/Node.java
@@ -1,9 +1,6 @@
 package tree.normal;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * @author ywxie
@@ -115,5 +112,20 @@ public class Node {
             depth++;
         }
         return depth;
+    }
+
+    /**
+     * 589. N叉树的前序遍历
+     */
+    List<Integer> res = new LinkedList<>();
+    public List<Integer> preorder(Node root) {
+        if (root == null){
+            return res;
+        }
+        res.add(root.val);
+        for (Node child: root.children){
+            preorder(child);
+        }
+        return res;
     }
 }
