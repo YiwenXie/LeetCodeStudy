0614250570853e18f1b40e77216c657cd73191d7
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2021-01-06 16:34:40 +0800

    669. 修剪二叉搜索树 DFS+迭代

--- src/tree/normal/BSTNode.java
+++ src/tree/normal/BSTNode.java
@@ -309,4 +309,55 @@ public class BSTNode {
         findModeHelper(node.right);
 
     }
+
+    /**
+     * 669. 修剪二叉搜索树
+     * DFS
+     */
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null){
+            return null;
+        }
+        if (root.val < low){
+            return trimBST(root.right, low, high);
+        }
+        if (root.val > high){
+            return trimBST(root.left, low, high);
+        }
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+        return root;
+    }
+
+    /**
+     * 669. 修剪二叉搜索树
+     * BFS
+     */
+    public TreeNode trimBSTByBFS(TreeNode root, int low, int high) {
+        if (root == null){
+            return root;
+        }
+        while (root.val < low || root.val > high){
+            if (root.val < low){
+                root = root.right;
+            }else {
+                root = root.left;
+            }
+        }
+        TreeNode cur = root;
+        while (cur != null){
+            while (cur.left != null && cur.left.val < low){
+                cur.left = cur.left.right;
+            }
+            cur = cur.left;
+        }
+        cur = root;
+        while (cur != null){
+            while (cur.right != null && cur.right.val > high){
+                cur.right = cur.right.left;
+            }
+            cur = cur.right;
+        }
+        return root;
+    }
 }
