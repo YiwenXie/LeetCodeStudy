98cb5c78efdb5344f152d92e8d5734bf9c77f13f
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2021-01-08 13:31:58 +0800

    814. 二叉树剪枝

--- src/tree/normal/TreeNode.java
+++ src/tree/normal/TreeNode.java
@@ -1528,4 +1528,20 @@ public class TreeNode {
         }
         return Math.max(left, right);
     }
+
+    /**
+     * 814. 二叉树剪枝
+     * 后序遍历，破除先序遍历的剪枝思想！
+     */
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null){
+            return null;
+        }
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        if(root.left == null && root.right == null && root.val == 0) {
+            return null;
+        }
+        return root;
+    }
 }
