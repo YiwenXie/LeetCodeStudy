43187ae3accad53d3cb32886a5a5851a68fadf29
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2021-01-11 15:24:08 +0800

    863. 二叉树中所有距离为 K 的结点

--- src/tree/normal/TreeNode.java
+++ src/tree/normal/TreeNode.java
@@ -1759,4 +1759,62 @@ public class TreeNode {
         ans = Math.max(ans, arrowLeft + arrowRight);
         return Math.max(arrowLeft, arrowRight);
     }
+
+    /**
+     * 863. 二叉树中所有距离为 K 的结点
+     */
+    Map<TreeNode, TreeNode> parentMap = new HashMap<>();
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
+        if (root == null){
+            return null;
+        }
+        distanceKDFS(root, null);
+        Queue<TreeNode> queue = new LinkedList();
+        queue.add(null);
+        queue.add(target);
+
+        Set<TreeNode> seen = new HashSet();
+        seen.add(target);
+        seen.add(null);
+
+        int dist = 0;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node == null) {
+                if (dist == K) {
+                    List<Integer> ans = new ArrayList();
+                    for (TreeNode n: queue) {
+                        ans.add(n.val);
+                    }
+                    return ans;
+                }
+                queue.offer(null);
+                dist++;
+            } else {
+                if (!seen.contains(node.left)) {
+                    seen.add(node.left);
+                    queue.offer(node.left);
+                }
+                if (!seen.contains(node.right)) {
+                    seen.add(node.right);
+                    queue.offer(node.right);
+                }
+                TreeNode par = parentMap.get(node);
+                if (!seen.contains(par)) {
+                    seen.add(par);
+                    queue.offer(par);
+                }
+            }
+        }
+        return new ArrayList<>();
+    }
+
+    private void distanceKDFS(TreeNode node, TreeNode parent){
+        if (node == null){
+            return;
+        }
+        parentMap.put(node, parent);
+        distanceKDFS(node.left, node);
+        distanceKDFS(node.right, node);
+    }
 }
