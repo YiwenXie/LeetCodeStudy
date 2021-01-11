14ec8be4dfa10d91332f14bf17cfbda7c76e0a4a
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2021-01-11 14:32:29 +0800

    337. 打家劫舍 III 动态规划

--- src/tree/normal/TreeNode.java
+++ src/tree/normal/TreeNode.java
@@ -1701,4 +1701,36 @@ public class TreeNode {
         }
         return false;
     }
+
+    /**
+     * 337. 打家劫舍 III
+     * 动态规划
+     */
+    public int rob(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        int[] res = robHelper(root);
+        return Math.max(res[0], res[1]);
+    }
+
+    private int[] robHelper(TreeNode node){
+        if (node == null){
+            return new int[]{0, 0};
+        }
+        // 分类讨论的标准是：当前结点偷或者不偷
+        // 由于需要后序遍历，所以先计算左右子结点，然后计算当前结点的状态值
+        int[] left = robHelper(node.left);
+        int[] right = robHelper(node.right);
+
+        // dp[0]：以当前 node 为根结点的子树能够偷取的最大价值，规定 node 结点不偷
+        // dp[1]：以当前 node 为根结点的子树能够偷取的最大价值，规定 node 结点偷
+
+        int[] dp = new int[2];
+        //如果当前结点不偷，左右子结点偷或者不偷都行，选最大者；
+        dp[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        //如果当前结点偷，左右子结点均不能偷。
+        dp[1] = node.val + left[0] + right[0];
+        return dp;
+    }
 }
