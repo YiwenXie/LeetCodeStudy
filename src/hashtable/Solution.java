bd00019b3f2fbd064da20dad11aa7e2bc66a0dc9
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2021-01-15 15:47:12 +0800

    202. 快乐数

--- src/hashtable/Solution.java
+++ src/hashtable/Solution.java
@@ -74,4 +74,35 @@ public class Solution {
 //        Integer[] nums3 = set.toArray(new Integer[set.size()]);
 //        return Arrays.stream(nums3).mapToInt(Integer::intValue).toArray();
     }
+
+    /**
+     * 202. 快乐数
+     * 对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和，
+     * 然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。如果 可以变为  1，那么这个数就是快乐数。
+     * 参考英文网站热评第一。这题可以用快慢指针的思想去做，有点类似于检测是否为环形链表那道题
+     * //如果给定的数字最后会一直循环重复，那么快的指针（值）一定会追上慢的指针（值），也就是
+     * //两者一定会相等。如果没有循环重复，那么最后快慢指针也会相等，且都等于1。
+     */
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+        do {
+            slow = toAddSquareNum(slow);
+            fast = toAddSquareNum(fast);
+            fast = toAddSquareNum(fast);
+            if (fast == 1){
+                return true;
+            }
+        }while (slow != fast);
+        return false;
+    }
+
+    private int toAddSquareNum(int m){
+        int sum = 0;
+        while (m != 0) {
+            sum += (m%10) * (m%10);
+            m = m/10;
+        }
+        return sum;
+    }
 }
