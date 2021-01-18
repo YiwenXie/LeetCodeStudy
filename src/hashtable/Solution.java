b593d78fdf7aae052286acc8065288fbba4ff858
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2021-01-18 14:32:10 +0800

    15. 三数之和

--- src/hashtable/Solution.java
+++ src/hashtable/Solution.java
@@ -225,4 +225,67 @@ public class Solution {
         }
         return true;
     }
+
+    /**
+     * 15. 三数之和
+     * 暴力法超时
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set result = new HashSet();
+        for(int i =0; i < nums.length; i++) {
+            for (int j = i +1; j< nums.length; j++) {
+                for(int l = j +1; l < nums.length; l++) {
+                    if (nums[i] + nums[j]+nums[l] == 0) {
+                        List result2 = new ArrayList();
+                        result2.add(nums[i]);
+                        result2.add(nums[j]);
+                        result2.add(nums[l]);
+                        Collections.sort(result2);
+                        result.add(result2);
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(result);
+    }
+
+    /**
+     * 15. 三数之和
+     * 指针法
+     */
+    public List<List<Integer>> threeSum2(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ls = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {  // 跳过可能重复的答案
+
+                int l = i + 1, r = nums.length - 1, sum = 0 - nums[i];
+                while (l < r) {
+                    if (nums[l] + nums[r] == sum) {
+                        ls.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                        while (l < r && nums[l] == nums[l + 1]) {
+                            l++;
+                        }
+                        while (l < r && nums[r] == nums[r - 1]) {
+                            r--;
+                        }
+                        l++;
+                        r--;
+                    } else if (nums[l] + nums[r] < sum) {
+                        while (l < r && nums[l] == nums[l + 1]) {
+                            l++;   // 跳过重复值
+                        }
+                        l++;
+                    } else {
+                        while (l < r && nums[r] == nums[r - 1]) {
+                            r--;
+                        }
+                        r--;
+                    }
+                }
+            }
+        }
+        return ls;
+    }
 }
