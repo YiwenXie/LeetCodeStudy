843bc6a9de28dab025a5d9a24356662ad708c470
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2021-01-19 14:52:03 +0800

    541. 反转字符串 II

--- src/str/StringSolution.java
+++ src/str/StringSolution.java
@@ -20,4 +20,34 @@ public class StringSolution {
             }
         }
     }
+
+    /**
+     * 541. 反转字符串 II
+     * 给定一个字符串 s 和一个整数 k，你需要对从字符串开头算起的每隔 2k 个字符的前 k 个字符进行反转。
+     *
+     *     如果剩余字符少于 k 个，则将剩余字符全部反转。
+     *     如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。
+     */
+    public String reverseStr(String s, int k) {
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < s.length(); i = 2 * k + i ){
+            if (i + k < s.length()) {
+                reverse(chars, i, i + k);
+            } else {
+                reverse(chars, i, s.length());
+            }
+        }
+        return String.valueOf(chars);
+    }
+
+    private void reverse(char[] chars, int start, int end){
+        int p = end - 1;
+        for (int i = start; i < end; i++){
+            if (i < p){
+                char temp = chars[i];
+                chars[i] = chars[p];
+                chars[p--] = temp;
+            }
+        }
+    }
 }
