be689160eb8c9b1ddd6dad3118f9414cf1525294
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2021-01-26 10:27:36 +0800

    225. 用队列实现栈

--- src/stackandqueue/Solution.java
+++ src/stackandqueue/Solution.java
@@ -1,5 +1,8 @@
 package stackandqueue;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Stack;
 
 /**
@@ -56,6 +59,39 @@ public class Solution {
                 return true;
             }
             return false;
+        }
+    }
+
+    /**
+     * 225. 用队列实现栈
+     */
+    class MyStack {
+
+        Deque<Integer> deque;
+
+        /** Initialize your data structure here. */
+        public MyStack() {
+            this.deque = new LinkedList<>();
+        }
+
+        /** Push element x onto stack. */
+        public void push(int x) {
+            deque.push(x);
+        }
+
+        /** Removes the element on top of the stack and returns that element. */
+        public int pop() {
+            return deque.pollFirst();
+        }
+
+        /** Get the top element. */
+        public int top() {
+            return deque.peekFirst();
+        }
+
+        /** Returns whether the stack is empty. */
+        public boolean empty() {
+            return deque.isEmpty();
         }
     }
 
