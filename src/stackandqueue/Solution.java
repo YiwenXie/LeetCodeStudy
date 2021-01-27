1d53faab849eab383bb64ee0a595c4d52c9a334e
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2021-01-27 13:53:47 +0800

    239. 滑动窗口最大值 单调队列

--- src/stackandqueue/Solution.java
+++ src/stackandqueue/Solution.java
@@ -174,4 +174,60 @@ public class Solution {
         return a / b;
     }
 
+    /**
+     * 单调队列
+     */
+    private class MonotonicQueue{
+
+        Deque<Integer> deque;
+
+        public MonotonicQueue() {
+            this.deque = new LinkedList<>();
+        }
+
+        // 每次弹出的时候，比较当前要弹出的数值是否等于队列出口元素的数值，如果相等则弹出。
+        // 同时pop之前判断队列当前是否为空。
+        void pop(int value){
+            if (!deque.isEmpty() && value == deque.peekFirst()){
+                deque.pollFirst();
+            }
+        }
+
+        // 如果push的数值大于入口元素的数值，那么就将队列后端的数值弹出，直到push的数值小于等于队列入口元素的数值为止。
+        // 这样就保持了队列里的数值是单调从大到小的了。
+        void push(int value){
+            while (!deque.isEmpty() && value > deque.peekLast()){
+                deque.pollLast();
+            }
+            deque.addLast(value);
+        }
+
+        // 查询当前队列里的最大值 直接返回队列前端也就是front就可以了。
+        int front(){
+            return deque.getFirst();
+        }
+    }
+
+    /**
+     * 239. 滑动窗口最大值
+     */
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if(nums == null||nums.length < 2) {
+            return nums;
+        }
+        MonotonicQueue queue = new MonotonicQueue();
+        int[] result = new int[nums.length - k + 1];
+        for (int i = 0; i < k; i++) { // 先将前k的元素放进队列
+            queue.push(nums[i]);
+        }
+        int j = 0;
+        result[j++] = queue.front(); // result 记录前k的元素的最大值
+        for (int i = k; i < nums.length; i++) {
+            queue.pop(nums[i - k]); // 滑动窗口移除最前面元素
+            queue.push(nums[i]); // 滑动窗口前加入最后面的元素
+            result[j++] = queue.front(); // 记录对应的最大值
+        }
+        return result;
+    }
+
 }
