e31ab6d593ffe3bbb48bedb775901a019dbb658c
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2021-02-01 14:01:46 +0800

    104. 二叉树的最大深度 递归+迭代

--- src/tree/Solution.java
+++ src/tree/Solution.java
@@ -179,4 +179,45 @@ public class Solution {
         }
         return true;
     }
+
+    /**
+     * 104. 二叉树的最大深度
+     * 递归
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null){
+            return 0;
+        }else {
+            int left = maxDepth(root.left);
+            int right = maxDepth(root.right);
+            return Math.max(left, right) + 1;
+        }
+    }
+
+    /**
+     * 104. 二叉树的最大深度
+     * 迭代
+     */
+    public int maxDepth2(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int depth = 0;
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            for (int i = 0; i < size; i++){
+                TreeNode node = queue.poll();
+                if (node.left != null){
+                    queue.add(node.left);
+                }
+                if (node.right != null){
+                    queue.add(node.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
+    }
 }
