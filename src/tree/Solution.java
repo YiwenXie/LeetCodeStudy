7c7df6b9e4328510b45be7a7ae1628f33752b232
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2021-02-01 14:15:38 +0800

    222. 完全二叉树的节点个数 递归

--- src/tree/Solution.java
+++ src/tree/Solution.java
@@ -220,4 +220,17 @@ public class Solution {
         }
         return depth;
     }
+
+    /**
+     * 222. 完全二叉树的节点个数
+     * 递归
+     */
+    public int countNodes(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        int left = countNodes(root.left);
+        int right = countNodes(root.right);
+        return left + right + 1;
+    }
 }
