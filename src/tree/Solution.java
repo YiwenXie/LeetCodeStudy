c89dcfb8f6322d73291c0b97b0fc910bacc9ea2d
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2021-02-02 14:01:31 +0800

    100. 相同的树

--- src/tree/Solution.java
+++ src/tree/Solution.java
@@ -233,4 +233,18 @@ public class Solution {
         int right = countNodes(root.right);
         return left + right + 1;
     }
+
+    /**
+     * 100. 相同的树
+     * 递归
+     */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null){
+            return true;
+        }
+        if (p == null || q == null){
+            return false;
+        }
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
 }
