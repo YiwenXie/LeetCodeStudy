bb1b94fc28c8989aada2689767f86dd124c75c52
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2023-10-24 21:55:14 +0800

    数据结构设计：字典树、前缀树、单词查找树

---
+++ src/SkillCategory/dataconstructdesing/trie/Solution.java
@@ -0,0 +1,10 @@
+package SkillCategory.dataconstructdesing.trie;
+
+/**
+ * @author ywxie
+ * @date 2022/6/20 17:36
+ * @describe Trie 树又叫字典树、前缀树、单词查找树，是一种二叉树衍生出来的高级数据结构，主要应用场景是处理字符串前缀相关的操作。
+ * 由于 Trie 树原理，我们要求 TrieMap<V> 的键必须是字符串类型，值的类型 V 可以随意。
+ */
+public class Solution {
+}
---
+++ src/SkillCategory/dataconstructdesing/trie/TrieNode.java
@@ -0,0 +1,20 @@
+package SkillCategory.dataconstructdesing.trie;
+
+/**
+ * @author ywxie
+ * @date 2022/6/20 17:37
+ * @describe TrieNode 节点本身只存储 val 字段，并没有一个字段来存储字符，
+ * 字符是通过子节点在父节点的 children 数组中的索引确定的。
+ * Trie 树用「树枝」存储字符串（键），用「节点」存储字符串（键）对应的数据（值）。
+ */
+public class TrieNode<V> {
+    /**
+     * 这个 val 字段存储键对应的值，children 数组存储指向子节点的指针。一个节点有 256 个子节点指针
+     * 但是和之前的普通多叉树节点不同，TrieNode 中 children 数组的索引是有意义的，代表键中的一个字符。
+     * 比如说 children[97] 如果非空，说明这里存储了一个字符 'a'，因为 'a' 的 ASCII 码为 97。
+     * 我们的模板只考虑处理 ASCII 字符，所以 children 数组的大小设置为 256。
+     **/
+    V val = null;
+    TrieNode<V>[] children = new TrieNode[256];
+
+}
