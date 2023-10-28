afe2fd7f07dbfcb36618f2f202f01402858d74c4
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2023-10-28 22:06:18 +0800

    Binary Search: [easy]35.SearchInsertPosition

---
+++ src/SkillCategory/array/BinarySearch/Easy35SearchInsertPosition.java
@@ -0,0 +1,22 @@
+package SkillCategory.array.BinarySearch;
+
+/**
+ * @author Yiwen Xie
+ * @description
+ * @date 2023/10/28 21:20
+ */
+public class Easy35SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
