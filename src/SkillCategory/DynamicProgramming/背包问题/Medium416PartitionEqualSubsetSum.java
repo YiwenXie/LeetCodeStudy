e692627e1fe9a2678bef89bec0b041c996511729
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2023-12-09 15:28:52 +0800

    DP: backpack problem

---
+++ src/SkillCategory/DynamicProgramming/背包问题/Medium416PartitionEqualSubsetSum.java
@@ -0,0 +1,160 @@
+package SkillCategory.DynamicProgramming.背包问题;
+
+/**
+ * @author Yiwen Xie
+ * @description
+ * @date 2023/12/9 9:24
+ */
+public class Medium416PartitionEqualSubsetSum {
+    /**
+     * Solution: Prefix sum
+     * 72 / 141 个通过的测试用例
+     * @param nums
+     * @return
+     */
+//    public boolean canPartition(int[] nums) {
+//        // sort array from small to large
+//        Arrays.sort(nums);
+//        // query array's prefix sum
+//        int n = nums.length;
+//        int[] prefixSum = new int[n + 1];
+//        for (int i = 1; i < n + 1; i++) {
+//            prefixSum[i] = nums[i - 1] + prefixSum[i - 1];
+//        }
+//        // iterate over this prefixSum array from 1st index
+//        for (int i = 1; i < n + 1; i++) {
+//            // query nums[0..i - 1]'s sum
+//            int a = prefixSum[i];
+//            // in this case we need to find out the difference that nums[0..n - 1] minus nums[0..i - 1]
+//            // index i partition nums array to two subset:1.nums[0..i - 1] 2.nums[i..n-1]
+//            // query nums[i..n-1]'s sum = nums[0..n - 1] - nums[0..i - 1] = prefixSum[n] - prefixSum[i]
+//            int b = prefixSum[n] - prefixSum[i];
+//            // judgement these subset is equals
+//            if (a == b){
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+
+    /**
+     * Solution: DP backpack
+     * Time Complexity: O(n * sum)
+     * Space Complexity: O(n * sum)
+     *
+     * @param nums
+     * @return
+     */
+    public boolean canPartition2(int[] nums) {
+        int n = nums.length;
+        // declare a variable that represents nums array' sum
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+        // if sum is odd not divided by 2, it cannot be divided into two subset that value is equal
+        if (sum % 2 != 0) {
+            return false;
+        }
+        // sum variable is represents the subset sum
+        sum = sum / 2;
+        // declare a two dimension array dp[n + 1][sum + 1]
+        // i is represents nums[i - 1]'s value
+        // j is represents package's capacity = one subset value
+        // for i items, current backpack's capacity is j
+        // in this case dp[i][j] is represents if backpack can divide into two equal subset
+        boolean[][] dp = new boolean[n + 1][sum + 1];
+        // base case is dp[..][0] = true and dp[0][..] = false，
+        // if backpack don't have capacity, that means backpack is filled
+        // if there don't have any item, backpack is empty
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = true;
+        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < sum + 1; j++) {
+                // backpack don't have enough capacity to put i item in backpack
+                if (j - nums[i - 1] < 0) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    // not put i item in backpack, so result is i - 1 's result
+                    boolean a = dp[i - 1][j];
+                    // put i item in backpack
+                    boolean b = dp[i - 1][j - nums[i - 1]];
+                    // have true is true, means backpack's capacity can divide into two p subset
+                    dp[i][j] = a || b;
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+
+    /**
+     * Solution: DP backpack
+     * Time Complexity: O(n\*sum)
+     * Space Complexity: O(sum)
+     *
+     * @param nums
+     * @return
+     */
+    public boolean canPartition3(int[] nums) {
+        int n = nums.length;
+        // declare a variable that represents nums array' sum
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+        // if sum is odd not divided by 2, it cannot be divided into two subset that value is equal
+        if (sum % 2 != 0) {
+            return false;
+        }
+        // sum variable is represents the divided subset sum
+        sum = sum / 2;
+        // dp[j] represents when backpack's capacity is j, backpack can or cannot be filled?
+        boolean[] dp = new boolean[sum + 1];
+        // base case
+        dp[0] = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = sum; j >= 0; j--) {
+                if (j - nums[i] >= 0) {
+                    dp[j] = dp[j] || dp[j - nums[i]];
+                }
+            }
+        }
+        // when backpack's capacity is sum, backpack can or cannot be filled?
+        return dp[sum];
+    }
+
+    /**
+     * Solution: DP backpack
+     * Time Complexity: O(n\*sum)
+     * Space Complexity: O(sum)
+     *
+     * @param nums
+     * @return
+     */
+    public boolean canPartition4(int[] nums) {
+        int n = nums.length;
+        // declare a variable that represents nums array' sum
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+        // if sum is odd not divided by 2, it cannot be divided into two subset that value is equal
+        if (sum % 2 != 0) {
+            return false;
+        }
+        // sum variable is represents the divided subset sum
+        sum = sum / 2;
+        // dp[j] represents when backpack's capacity is j, the maximum value that backpack can be filled
+        int[] dp = new int[sum + 1];
+        // base case
+        dp[0] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = sum; j >= nums[i]; j--) {
+                dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
+            }
+        }
+        // when backpack's capacity is sum, backpack can or cannot be filled?
+        return dp[sum] == sum;
+    }
+}
