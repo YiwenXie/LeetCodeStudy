ba0b48ec54753736e3ecf374f90916e7942b12b1
ywxie <yiwenzxyzhh@users.noreply.github.com>
2024-01-07 17:36:12 +0800

    add merge sort solution

---
+++ src/SkillCategory/Sort/Medium912_SortAnArray_MergeSort.java
@@ -0,0 +1,40 @@
+package SkillCategory.Sort;
+
+public class Medium912_SortAnArray_MergeSort {
+    private static int[] temp;
+
+    public int[] sortArray(int[] nums) {
+        temp = new int[nums.length];
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private void mergeSort(int[] nums, int lo, int hi) {
+        if (lo >= hi) {
+            return;
+        }
+        int mid = lo + (hi - lo) / 2;
+        mergeSort(nums, lo, mid);
+        mergeSort(nums, mid, hi);
+        toMerge(nums, lo, mid, hi);
+    }
+
+    private void toMerge(int[] nums, int lo, int mid, int hi) {
+        for (int i = lo; i <= hi; i++) {
+            temp[i] = nums[i];
+        }
+        int i = lo;
+        int j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            if (i >= mid + 1) {
+                nums[k] = temp[j++];
+            } else if (j >= hi + 1) {
+                nums[k] = temp[i++];
+            } else if (temp[i] > temp[j]) {
+                nums[k] = temp[j++];
+            } else {
+                nums[k] = temp[i++];
+            }
+        }
+    }
+}
