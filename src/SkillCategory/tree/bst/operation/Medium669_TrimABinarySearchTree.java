1f9d236c5f1ed288acc80adc94492e2244751a83
ywxie <30338551+yiwenzxyzhh@users.noreply.github.com>
2024-02-22 20:04:16 +0800

    add tree problem solution medium669

---
+++ src/SkillCategory/tree/bst/operation/Medium669_TrimABinarySearchTree.java
@@ -0,0 +1,21 @@
+package SkillCategory.tree.bst.operation;
+
+import SkillCategory.tree.TreeNode;
+
+public class Medium669_TrimABinarySearchTree {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val > high) {
+            return trimBST(root.left, low, high);
+        }
+        if (root.val < low) {
+            return trimBST(root.right, low, high);
+        }
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+        return root;
+    }
+
+}
