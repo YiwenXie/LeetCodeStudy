191e1ad7dc1e4b671b1efb66501049ae19931a31
ywxie <30338551+yiwenzxyzhh@users.noreply.github.com>
2024-02-24 21:09:05 +0800

    optimal tree solution easy108

--- src/SkillCategory/tree/bst/construct/Easy108_ConvertSortedArrayToBinarySearchTree.java
+++ src/SkillCategory/tree/bst/construct/Easy108_ConvertSortedArrayToBinarySearchTree.java
@@ -5,6 +5,7 @@ import SkillCategory.tree.TreeNode;
 public class Easy108_ConvertSortedArrayToBinarySearchTree {
     public TreeNode sortedArrayToBST(int[] nums) {
         return recursion(nums, 0, nums.length);
+//        return recursion2(nums, 0, nums.length - 1);
     }
 
     private TreeNode recursion(int[] nums, int lo, int hi) {
@@ -15,6 +16,17 @@ public class Easy108_ConvertSortedArrayToBinarySearchTree {
         TreeNode root = new TreeNode(nums[mid]);
         root.left = recursion(nums, lo, mid);
         root.right = recursion(nums, mid + 1, hi);
+        return root;
+    }
+
+    private TreeNode recursion2(int[] nums, int lo, int hi) {
+        if (lo > hi) {
+            return null;
+        }
+        int mid = lo + (hi - lo) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = recursion2(nums, lo, mid - 1);
+        root.right = recursion2(nums, mid + 1, hi);
         return root;
     }
 }
