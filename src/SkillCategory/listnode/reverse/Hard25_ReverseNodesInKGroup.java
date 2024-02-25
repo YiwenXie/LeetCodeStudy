9ad540f561c694b6dbd94e65014ef16f8370c98f
ywxie <30338551+yiwenzxyzhh@users.noreply.github.com>
2024-02-25 12:05:24 +0800

    optimal list node solution hard25

--- src/SkillCategory/listnode/reverse/Hard25_ReverseNodesInKGroup.java
+++ src/SkillCategory/listnode/reverse/Hard25_ReverseNodesInKGroup.java
@@ -45,4 +45,27 @@ public class Hard25_ReverseNodesInKGroup {
         head.next = null;
         return last;
     }
+
+    public ListNode reverseKGroup2(ListNode head, int k) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode end = dummy;
+        while (end.next != null) {
+            for (int i = 0; i < k && end != null; i++) {
+                end = end.next;
+            }
+            if (end == null) {
+                break;
+            }
+            ListNode start = pre.next;
+            ListNode next = end.next;
+            end.next = null;
+            pre.next = reverse(start);
+            start.next = next;
+            pre = start;
+            end = pre;
+        }
+        return dummy.next;
+    }
 }
