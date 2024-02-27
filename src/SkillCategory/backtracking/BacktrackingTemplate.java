6b09fff5d1145d99a87980dcae4f1046d31ea057
ywxie <30338551+yiwenzxyzhh@users.noreply.github.com>
2024-02-27 19:15:18 +0800

    add backtracking template

---
+++ src/SkillCategory/backtracking/BacktrackingTemplate.java
@@ -0,0 +1,22 @@
+package SkillCategory.backtracking;
+
+/**
+ * 回溯法一般是在集合中递归搜索，集合的大小构成了树的宽度，递归的深度构成的树的深度。
+ * for循环可以理解是横向遍历，backtracking（递归）就是纵向遍历，
+ * 这样就把这棵树全遍历完了，一般来说，搜索叶子节点就是找的其中一个结果了。
+ */
+public class BacktrackingTemplate {
+//    pulic void backtracking(参数) {
+//        if (终止条件) {
+//            存放结果;
+//            return;
+//        }
+//
+//        for (选择：本层集合中元素（树中节点孩子的数量就是集合的大小）) {
+//            处理节点;
+//            backtracking(路径，选择列表); // 递归
+//            回溯，撤销处理结果
+//        }
+//    }
+
+}
