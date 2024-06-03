b544baee13d45ec95fc827e3991f19c1aab715c3
ywxie <30338551+yiwenzxyzhh@users.noreply.github.com>
2024-06-03 20:54:41 +0800

    update tree problem solution - medium652

--- src/SkillCategory/tree/normal/field/Medium652_FindDuplicateSubtrees.java
+++ src/SkillCategory/tree/normal/field/Medium652_FindDuplicateSubtrees.java
@@ -10,37 +10,38 @@ public class Medium652_FindDuplicateSubtrees {
     List<TreeNode> result;
     HashMap<String, Integer> map;
 
+    /**
+     * 时间复杂度：DFS 过程复杂度为 O(n)，对于每个子树需要构造出与子树同等规模的字符串，复杂度为 O(n)。
+     * 整体复杂度为 O(n^2)
+     * 空间复杂度：O(n)
+     *
+     * @param root
+     * @return
+     */
     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-//        result = new HashSet<>();
+        map = new HashMap<>();
         result = new ArrayList<>();
-        List<TreeNode> list = new ArrayList<>();
-        if (root == null) {
-            return list;
-        }
-        compare(root.left, root.right);
-        list.addAll(result);
-        return list;
+        dfs(root);
+        return result;
     }
 
-    private boolean compare(TreeNode node1, TreeNode node2) {
-        if (node1 == null && node2 == null) {
-            return false;
+    /**
+     * 设计递归函数 String dfs(TreeNode root)，含义为返回以传入参数 root 为根节点的子树所对应的指纹标识。
+     *
+     * @param root
+     * @return
+     */
+    private String dfs(TreeNode root) {
+        // 对于标识的设计只需使用 "_" 分割不同的节点值，同时对空节点进行保留（定义为空串 " "）即可。
+        if (root == null) {
+            return " ";
         }
-        if (node1 == null) {
-            return false;
+        String key = root.val + "-" + dfs(root.left) + dfs(root.right);
+        map.put(key, map.getOrDefault(key, 0) + 1);
+        // 使用哈希表记录每个标识（子树）出现次数，当出现次数为 2（首次判定为重复出现）时，将该节点加入答案。
+        if (map.get(key) == 2) {
+            result.add(root);
         }
-        if (node2 == null) {
-            return false;
-        }
-        if (node1.val != node2.val) {
-            return compare(node1.left, node2) && compare(node1.right, node2)
-                    && compare(node1, node2.left) && compare(node1, node2.right);
-        }
-        boolean left = compare(node1.left, node2.left);
-        boolean right = compare(node1.right, node2.right);
-        if (left && right) {
-            result.add(node1);
-        }
-        return true;
+        return key;
     }
 }
